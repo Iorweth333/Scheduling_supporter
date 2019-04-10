058fc5b68fe482a0d1f63cc5b7c7124868a42086
kstopyra <kamil.stopyra95@gmail.com>
2019-04-10 16:40:03 +0200

    Changed scheduler component

--- frontend/src/components/SchedulerCalendar.js
+++ frontend/src/components/SchedulerCalendar.js
@@ -1,43 +1,181 @@
-import React from 'react'
+import React, { Component } from "react";
+import moment from "moment";
+import 'react-calendar-timeline/lib/Timeline.css'
+import Timeline from "react-calendar-timeline";
 
-import FullCalendar from 'fullcalendar-reactwrapper';
-import "fullcalendar-reactwrapper/dist/css/fullcalendar.min.css";
-import dayGridPlugin from 'fullcalendar-reactwrapper';
-import timeGridPlugin from 'fullcalendar-reactwrapper';
-import interactionPlugin from 'fullcalendar-reactwrapper';
 
-export default class SchedulerCalendar extends React.Component {
+var keys = {
+    groupIdKey: "id",
+    groupTitleKey: "title",
+    groupRightTitleKey: "rightTitle",
+    itemIdKey: "id",
+    itemTitleKey: "title",
+    itemDivTitleKey: "title",
+    itemGroupKey: "group",
+    itemTimeStartKey: "start",
+    itemTimeEndKey: "end",
+    groupLabelKey: "title"
+};
 
-    constructor(props){
+export default class SchedulerCalendar extends Component {
+    constructor(props) {
         super(props);
+
+        this.state = {
+            groups: [{ id: 1, title: 'CS', root: true },
+                { id: 4, title: 'Jan Nowak', root: false, parent: 1},
+                { id: 5, title: 'Pawel Kowalski', root: false, parent: 1},
+                { id: 6, title: 'Zbigniew Bak', root: false, parent: 1},
+                { id: 2, title: 'E&T', root: true },
+                { id: 7, title: 'Marek Garek', root: false, parent: 2},
+                { id: 8, title: 'Pawel Kowalski', root: false, parent: 2}],
+            items:
+                [
+                {
+                    id: 1,
+                    group: 4,
+                    title: "WDI",
+                    start: moment().add(-8, 'hour'),
+                    end: moment().add(-6, 'hour'),
+                    className: (moment(moment()).day() === 6 || moment(moment()).day() === 0) ? 'item-weekend' : '',
+                    itemProps: {
+                        'data-tip': "WDI"
+                    }
+                },
+                {
+                    id: 2,
+                    group: 5,
+                    title: "ASD",
+                    start: moment().add(-5, 'hour'),
+                    end: moment().add(-3, 'hour'),
+                    className: (moment(moment()).day() === 6 || moment(moment()).day() === 0) ? 'item-weekend' : '',
+                    itemProps: {
+                        'data-tip': "ASD"
+                    }
+                },
+                {
+                    id: 3,
+                    group: 6,
+                    title: "Funkcyjne",
+                    start: moment().add(1, 'hour'),
+                    end: moment().add(3, 'hour'),
+                    className: (moment(moment()).day() === 6 || moment(moment()).day() === 0) ? 'item-weekend' : '',
+                    itemProps: {
+                        'data-tip': "Funkcyjne"
+                    }
+                },
+                {
+                    id: 4,
+                    group: 7,
+                    title: "JKA",
+                    start: moment().add(-8, 'hour'),
+                    end: moment().add(-6, 'hour'),
+                    className: (moment(moment()).day() === 6 || moment(moment()).day() === 0) ? 'item-weekend' : '',
+                    itemProps: {
+                        'data-tip': "JKA"
+                    }
+                }
+            ],
+            defaultTimeStart: moment().startOf("day").toDate(),
+            defaultTimeEnd: moment().startOf("day").add(1, "day").toDate(),
+            openGroups: {}
+        };
     }
 
-    handleDateClick(arg){
-        this.setState({  // add new event data
-            calendarEvents: this.state.calendarEvents.concat({ // creates a new array
-                title: 'New Event',
-                start: arg.date,
-                allDay: arg.allDay
-            })
-        })
-    }
+    toggleGroup = id => {
+        const { openGroups } = this.state;
+        this.setState({
+            openGroups: {
+                ...openGroups,
+                [id]: !openGroups[id]
+            }
+        });
+    };
 
-    render(){
-        return(
-            <FullCalendar
-                id = "your-custom-ID"
-                header = {{
-                    left: 'prev,next today myCustomButton',
-                    center: 'title',
-                    right: 'month,basicWeek,basicDay'
-                }}
-                {...this.props}
-                defaultDate={'2019-04-08'}
-                plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
-                navLinks= {true} // can click day/week names to navigate views
-                editable= {true}
-                eventLimit= {true} // allow "more" link when too many events
-                dateClick={ this.handleDateClick }
+
+    handleItemMove = (itemId, dragTime, newGroupOrder) => {
+        const { items, groups } = this.state;
+
+        const group = groups[newGroupOrder];
+
+        this.setState({
+            items: items.map(item =>
+                item.id === itemId
+                    ? Object.assign({}, item, {
+                        start: dragTime,
+                        end: dragTime + (item.end - item.start),
+                        group: group.id
+                    })
+                    : item
+            )
+        });
+
+        console.log("Moved", itemId, dragTime, newGroupOrder);
+    };
+
+    handleItemResize = (itemId, time, edge) => {
+        const { items } = this.state;
+
+        this.setState({
+            items: items.map(item =>
+                item.id === itemId
+                    ? Object.assign({}, item, {
+                        start: edge === "left" ? time : item.start,
+                        end: edge === "left" ? item.end : time
+                    })
+                    : item
+            )
+        });
+
+        console.log("Resized", itemId, time, edge);
+    };
+
+    render() {
+        const {
+            groups,
+            items,
+            defaultTimeStart,
+            defaultTimeEnd,
+            openGroups
+        } = this.state;
+
+       const newGroups = groups
+            .filter(g => g.root || openGroups[g.parent])
+            .map(group => {
+                return Object.assign({}, group, {
+                    title: group.root ? (
+                        <div
+                            onClick={() => this.toggleGroup(parseInt(group.id))}
+                            style={{ cursor: "pointer" }}
+                        >
+                            {openGroups[parseInt(group.id)] ? "[-]" : "[+]"} {group.title}
+                        </div>
+                    ) : (
+                        <div style={{ paddingLeft: 20 }}>{group.title}</div>
+                    )
+                });
+            });
+
+        return (
+            <Timeline
+                groups={newGroups}
+                items={items}
+                keys={keys}
+                fixedHeader="fixed"
+                sidebarWidth={150}
+                sidebarContent={<div></div>}
+                canMove
+                canResize="right"
+                canSelect
+                itemsSorted
+                itemTouchSendsClick={false}
+                stackItems
+                itemHeightRatio={0.75}
+                showCursorLine
+                defaultTimeStart={defaultTimeStart}
+                defaultTimeEnd={defaultTimeEnd}
+                onItemMove={this.handleItemMove}
+                onItemResize={this.handleItemResize}
             />
         );
     }
--- frontend/src/components/Upload.js
+++ frontend/src/components/Upload.js
@@ -4,7 +4,6 @@ import {connect} from 'react-redux';
 import {uploadFile} from "../actions";
 import SchedulerCalendar from "./SchedulerCalendar";
 
-
 const UploadFile = ({ input: {value: omitValue, ...inputProps }, meta: omitMeta, ...props }) => (
     <input type='file' {...inputProps} {...props} />
 );
@@ -14,39 +13,6 @@ class Upload extends Component{
 
     constructor (props) {
         super(props);
-        this.state = {
-            events:[
-                {
-                    title: 'All Day Event',
-                    start: '2019-04-08'
-                },
-                {
-                    id: 997,
-                    title: 'Jjsajajj',
-                    start: '2019-04-01T16:00:00'
-                },
-                {
-                    id: 999,
-                    title: 'Ababba',
-                    start: '2019-04-01T16:00:00'
-                },
-                {
-                    title: 'Conference',
-                    start: '2017-05-11',
-                    end: '2017-05-13'
-                },
-                {
-                    title: 'Meeting',
-                    start: '2019-04-12T10:30:00',
-                    end: '2019-04-12T12:30:00'
-                },
-                {
-                    title: 'Click for Google',
-                    url: 'http://google.com/',
-                    start: '2019-04-14'
-                }
-            ],
-        };
     }
 
     render(){
@@ -60,14 +26,11 @@ class Upload extends Component{
                         URI: {this.props.calendar.fileDownloadUri}
                         <br/>
                         SIZE: {this.props.calendar.size}
-
-                        <SchedulerCalendar events={this.state.events}/>
+                        <SchedulerCalendar/>
                     </div>
                 ) : (
                     <form onSubmit={handleSubmit(this.props.uploadFile)}>
-
                         <Field component={UploadFile} name='scheduleUpload' accept=".png,.csv,.xls,.xlsx" />
-
                         <button type="submit">Submit</button>
                     </form>
                 )}
