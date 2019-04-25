2eb7d4f72921cda83f60c18efe82a7a5a20920a9
malina <jmalinaa@gmail.com>
2019-04-25 14:37:04 +0200

    Created endpoint for conflicts

---
+++ backend/src/main/java/ioiobagiety/controller/ConflictsFinderController.java
@@ -0,0 +1,31 @@
+package ioiobagiety.controller;
+
+import ioiobagiety.exception.XLSXParseException;
+import ioiobagiety.service.LessonService;
+import ioiobagiety.util.XLSXParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@RestController
+@RequestMapping(value = "/conflicts")
+public class ConflictsFinderController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConflictsFinderController.class);
+
+    @Autowired
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<String> ConflictsList() {
+
+        List<Conflicts> conflicts = ConflictsFinderService.getAll();
+
+        if (conflicts.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(gson.toJson(conflicts), HttpStatus.OK);
+        }
+    }
+}
---
+++ backend/src/main/java/ioiobagiety/service/ConflictsFinderService.java
@@ -0,0 +1,6 @@
+
+public interface HelloIOBagietyService {
+
+    List<Conflicts> getAll() throws ResourceNotFoundException;
+
+}
