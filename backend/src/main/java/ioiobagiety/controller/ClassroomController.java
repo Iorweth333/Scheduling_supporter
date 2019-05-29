4fcf98d607c8e63d09062d5401a6d06d19429db8
Malenczuk <malen11pl@gmail.com>
2019-05-29 11:02:54 +0200

    [IOIOB-35] Add cross-origin to endpoints

--- backend/src/main/java/ioiobagiety/controller/ClassroomController.java
+++ backend/src/main/java/ioiobagiety/controller/ClassroomController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,6 +31,7 @@ public class ClassroomController {
     @Autowired
     private Gson gson;
 
+    @CrossOrigin(maxAge = 3600)
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<String> getClassrooms() {
         List<Classroom> classrooms;
@@ -41,6 +43,7 @@ public class ClassroomController {
         return new ResponseEntity<>(gson.toJson(classrooms), HttpStatus.OK);
     }
 
+    @CrossOrigin(maxAge = 3600)
     @RequestMapping(value = "/{classroomId}", method = RequestMethod.GET)
     public ResponseEntity<String> getClassroom(@PathVariable("classroomId") long classroomId) {
         Classroom classroom;
@@ -52,6 +55,7 @@ public class ClassroomController {
         return new ResponseEntity<>(gson.toJson(classroom), HttpStatus.OK);
     }
 
+    @CrossOrigin(maxAge = 3600)
     @RequestMapping(value = "/{classroomId}/lessons", method = RequestMethod.GET)
     public ResponseEntity<String> getLessons(@PathVariable("classroomId") long classroomId) {
         List<Lesson> lessons;
